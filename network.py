66b2b1d60d071e7b874108def7932f7c0bb98d42
Vetle Birkeland Huglen <vetlebh@Vetles-MacBook-Pro.local>
2020-07-20 11:22:43 +0200

    added all

--- network.py
+++ network.py
@@ -2,17 +2,13 @@
 import numpy as np
 
 import tensorflow as tf
-from tensorflow_graphics.math.interpolation import bspline
 from tensorflow import keras
-from tensorflow.keras.losses import BinaryCrossentropy
-from tensorflow.keras.models import Model, load_model
-from tensorflow.keras.layers import Input, BatchNormalization, Activation, Dense, Dropout
-from tensorflow.keras.layers import Lambda, RepeatVector, Reshape, Flatten
-from tensorflow.keras.layers import Conv2D, Conv2DTranspose, Dense
-from tensorflow.keras.layers import MaxPooling2D, GlobalMaxPool2D
-from tensorflow.keras.layers import concatenate, add
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
-from tensorflow.keras.optimizers import Adam
+
+from tensorflow.keras.models import Sequential ,Model
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import LeakyReLU
 
 
 
@@ -59,79 +55,27 @@ class pixelizedIoU(keras.losses.Loss):
         return ce
 
 
-
-def conv2d_block(input_tensor, n_filters, kernel_size = 3, batchnorm = True):
-    """Function to add 2 convolutional layers with the parameters passed to it"""
-    # first layer
-    x = Conv2D(filters = n_filters, kernel_size = (kernel_size, kernel_size),              kernel_initializer = 'he_normal', padding = 'same')(input_tensor)
-    if batchnorm:
-        x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-
-    """
-    # second layer
-    x = Conv2D(filters = n_filters, kernel_size = (kernel_size, kernel_size),\
-              kernel_initializer = 'he_normal', padding = 'same')(x)
-    if batchnorm:
-        x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    """
-
-    return x
-
-
-def network(input_img, n_filters = 16, dropout = 0.1, batchnorm = True):
-
-    c1 = conv2d_block(input_img, n_filters * 1, kernel_size = 3, batchnorm = batchnorm)
-    p1 = MaxPooling2D((2, 2))(c1)
-    p1 = Dropout(dropout)(p1)
-
-    c2 = conv2d_block(p1, n_filters * 2, kernel_size = 3, batchnorm = batchnorm)
-    p2 = MaxPooling2D((2, 2))(c2)
-    p2 = Dropout(dropout)(p2)
-
-    c3 = conv2d_block(p2, n_filters * 4, kernel_size = 3, batchnorm = batchnorm)
-    p3 = MaxPooling2D((2, 2))(c3)
-    p3 = Dropout(dropout)(p3)
-
-    c4 = conv2d_block(p3, n_filters * 8, kernel_size = 3, batchnorm = batchnorm)
-    p4 = MaxPooling2D((2, 2))(c4)
-    p4 = Dropout(dropout)(p4)
-
-    c5 = conv2d_block(p4, n_filters = n_filters * 16, kernel_size = 3, batchnorm = batchnorm)
-    p5 = MaxPooling2D((2, 2))(c5)
-    p5 = Dropout(dropout)(p5)
-
-    #outputs = Conv2D(1, (1, 1), activation='sigmoid')(c5)
-
-
-    f = Flatten()(p5)
-
-    d = Dense(16, activation='sigmoid')(f)
-
-    outputs = d * 255.9
-    model = Model(inputs=[input_img], outputs=[outputs])
+def convNet(output_size, im_height, im_width):
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3),activation='linear',input_shape=(im_height,im_width,1),padding='same'))
+    model.add(LeakyReLU(alpha=0.1))
+    model.add(MaxPooling2D((2, 2),padding='same'))
+    model.add(Conv2D(64, (3, 3), activation='linear',padding='same'))
+    model.add(LeakyReLU(alpha=0.1))
+    model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+    model.add(Conv2D(128, (3, 3), activation='linear',padding='same'))
+    model.add(LeakyReLU(alpha=0.1))
+    model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+    model.add(Conv2D(256, (3, 3), activation='linear',padding='same'))
+    model.add(LeakyReLU(alpha=0.1))
+    model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+    model.add(Flatten())
+    model.add(Dense(256, activation='linear'))
+    model.add(LeakyReLU(alpha=0.1))
+    model.add(Dense(output_size, activation='sigmoid'))
     return model
 
 
-fashion_model = Sequential()
-fashion_model.add(Conv2D(32, kernel_size=(3, 3),activation='linear',input_shape=(28,28,1),padding='same'))
-fashion_model.add(LeakyReLU(alpha=0.1))
-fashion_model.add(MaxPooling2D((2, 2),padding='same'))
-fashion_model.add(Conv2D(64, (3, 3), activation='linear',padding='same'))
-fashion_model.add(LeakyReLU(alpha=0.1))
-fashion_model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
-fashion_model.add(Conv2D(128, (3, 3), activation='linear',padding='same'))
-fashion_model.add(LeakyReLU(alpha=0.1))
-fashion_model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
-fashion_model.add(Flatten())
-fashion_model.add(Dense(128, activation='linear'))
-fashion_model.add(LeakyReLU(alpha=0.1))
-fashion_model.add(Dense(num_classes, activation='softmax'))
-
-
-def get_model(im_height=256, im_width=256, n_filters=16, dropout=0.05, batchnorm=True):
-    input_img = Input((im_height, im_width, 1), name='img')
-    model = network(input_img, n_filters=n_filters, dropout=dropout, batchnorm=batchnorm)
-    #model.compile(optimizer=Adam(), loss=pixelizedIoU(), metrics=["accuracy"])
+def get_model(output_size=16, im_height=256, im_width=256):
+    model = convNet(output_size, im_height, im_width)
     return model
--- spline.py
+++ spline.py
@@ -127,3 +127,106 @@ class Spline:
         plt.figure()
         plt.imshow(self.img, origin='lower')
         plt.show()
+
+
+
+
+
+
+
+
+
+def calc_stepsize(nd, d, p_i, delta_t):
+    """
+    Based on theroretical estimate, see report
+    """
+    #return 1.0/((nd+d)*np.max(np.sum(p_i, axis=0)))
+    return delta_t/(np.sum(np.sqrt(np.sum(np.square(p_i), axis=1))))
+
+
+
+def calc_intersect(point):
+    #cell_height = point[1]-int(point[1])
+    #return cell_height*255.0
+    return 1.0
+
+
+def floodFill_start(img, row):
+    """
+    Should be cleaned
+    """
+    """
+    Iterates outwards from start row to find point satisfied by
+    ray-casting algortihm to be inside spline
+    """
+    if (row >= 256 or row < 0):
+        return False
+    next_row = int(row + 10)
+    intersect = np.where(img[row, :] > 0.0)[0]
+    nr_intersect = intersect.shape[0]
+    if (nr_intersect > 0):
+        if (intersect[0] > 0):
+            return (0, row)
+        else:
+            for i in range (0, nr_intersect-2, 2):
+                if (intersect[i+2]-intersect[i+1] > 1):
+                    return (int((intersect[i+1]+intersect[i])/2), row)
+        return floodFill_start(img, next_row)
+    else:
+        return floodFill_start(img, next_row)
+
+
+def floodFill_(img):
+    img = np.pad(img, 1, 'constant', constant_values=(0))
+    start_ind = floodFill_start(img, 0)
+    if not start_ind:
+        img[:,:] = 0
+    else:
+        img = img.astype('uint8')
+        img = floodFill(img, None, start_ind, 1.0)[1]
+        cond = np.where((img==0) | (img==1))
+        img[cond] = 1-img[cond]
+    img = img.astype('float32')
+    img[np.where(img > 1)] = img[np.where(img > 1)]/255.0
+    img = img[1:-1, 1:-1]
+    return img
+
+
+def get_spline(p, flood=True):
+
+    d = 3
+    rows = 256
+    cols = 256
+
+    n = p.shape[0]
+    p_temp = np.zeros((n+d,2))
+    p_temp[:n] = p
+    p_temp[n:] = p[:d]
+    p = p_temp
+    n = n+d
+
+    t = np.linspace(0, 1, n+d+1)
+    delta_t = 1.0/(n+d)
+    spline = BSpline(t, p, d, 'periodic')
+
+    # Calculate step size
+    """
+    Based on theroretical estimate, see report
+    """
+    step1 = 1/((n+d)*np.max(np.sum(p, axis=0)))
+    step = delta_t/(np.sum(np.sqrt(np.sum(np.square(p), axis=1))))
+    print(f'stepsize = {step, step1}')
+
+    img = np.zeros((rows, cols))
+    t_n = np.linspace(0, 1, int(1.0/step))
+    print(spline(t_n).astype(int))
+    for x in t_n:
+        point = spline(x)
+        col = int(point[0])
+        row = int(point[1])
+        if img[row, col] == 0:
+            img[row, col] = calc_intersect(point)
+
+    img = floodFill_(img)
+
+    return img
